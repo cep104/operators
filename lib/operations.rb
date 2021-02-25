372f519dd29a8e54102c7903f2fab7286f634f21
cep104 <candicelizabethpeters@gmail.com>
2021-02-25 15:36:01 -0600

    commit tree 73ad90ec91fc043e8db23af4adecd44f3295f7ff

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,7 +5,10 @@ end
 
 
 def not_safe?(speed)
-	
+	#if it is less than 40 return true
+    #if it more than 60 return true 
+    #age < 2 ? "baby" : "not a baby"
+    speed < 40 || speed > 60 ? true : false 
 end
 	
 
